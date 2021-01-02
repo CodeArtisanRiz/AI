a20f638fee811c9b3a7f180627e02209ef1aab0a
xtremist96 <riz1.xtreme@gmail.com>
2021-01-02 21:43:47 +0530

    First comit

---
+++ app.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function() {
+    console.log("You can speak now.");
+};
+
+recognition.onresult = function(event) {
+    // console.log(event);
+    const current = event.resultIndex;
+
+    const transcript = event.results[current][0].transcript;
+    content.textContent = "Output: " + transcript;
+    readOutLoud(transcript);
+
+};
+
+btn.addEventListener('click', () => {
+    recognition.start();
+});
+
+function readOutLoud(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = .5;
+
+    window.speechSynthesis.speak(speech);
+
+}
