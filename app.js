8243bff5a90e31a98f2c565f36e1aaa7e5fedfd5
xtremist96 <riz1.xtreme@gmail.com>
2022-01-08 00:59:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,50 @@
-// const btn = document.querySelector('.talk');
-// const content = document.querySelector('.content');
+const btn = document.querySelector(".talk");
+const content = document.querySelector(".content");
 
-// const greetings = [
-//     'I am good, how about you',
-//     ' I am doing fine what about you',
-//     'I am good, now leave me alone'
-// ];
+const greetings = [
+  "I am good, how about you",
+  " I am doing fine what about you",
+  "I am good, now leave me alone",
+];
 
-// const weather = [
-//     'weather is fine',
-//     'why? you dont go out',
-//     'why do you care?'
-// ]
+const weather = ["weather is fine", "why? you dont go out", "why do you care?"];
 
-// const SpeechRecognition =
-//     window.SpeechRecognition || window.webkitSpeechRecognition;
-// const recognition = new SpeechRecognition();
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
-// recognition.onstart = function() {
-//     console.log("You can speak now.");
-// };
+recognition.onstart = function () {
+  console.log("You can speak now.");
+};
 
-// recognition.onresult = function(event) {
-//     // console.log(event);
-//     const current = event.resultIndex;
+recognition.onresult = function (event) {
+  // console.log(event);
+  const current = event.resultIndex;
 
-//     const transcript = event.results[current][0].transcript;
-//     content.textContent = "Output: " + transcript;
-//     readOutLoud(transcript);
+  const transcript = event.results[current][0].transcript;
+  content.textContent = "Output: " + transcript;
+  readOutLoud(transcript);
+};
 
-// };
+btn.addEventListener("click", () => {
+  recognition.start();
+});
 
-// btn.addEventListener('click', () => {
-//     recognition.start();
-// });
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = "I dont know";
 
-// function readOutLoud(message) {
+  if (message.includes("how are you")) {
+    const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+    speech.text = finalText;
+  } else if (message.includes("weather")) {
+    const finalText = weather[Math.floor(Math.random() * weather.length)];
+    speech.text = finalText;
+  }
 
-//     const speech = new SpeechSynthesisUtterance();
-//     speech.text = 'I dont know';
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 0.5;
 
-//     if (message.includes('how are you')) {
-//         const finalText =
-//             greetings[Math.floor(Math.random() * greetings.length)];
-//         speech.text = finalText;
-//     } else if (message.includes('weather')) {
-//         const finalText =
-//             weather[Math.floor(Math.random() * weather.length)];
-//         speech.text = finalText;
-//     }
-
-//     speech.volume = 1;
-//     speech.rate = 1;
-//     speech.pitch = .5;
-
-//     window.speechSynthesis.speak(speech);
-
-// }
+  window.speechSynthesis.speak(speech);
+}
