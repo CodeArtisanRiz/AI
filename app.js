10e366077913373bbef0b502e36ab9d1eba1575f
xtremist96 <riz1.xtreme@gmail.com>
2022-01-08 00:13:35 +0530

    2.0

--- app.js
+++ app.js
@@ -1,63 +1,59 @@
-const btn = document.querySelector('.talk');
-const content = document.querySelector('.content');
+// const btn = document.querySelector('.talk');
+// const content = document.querySelector('.content');
 
+// const greetings = [
+//     'I am good, how about you',
+//     ' I am doing fine what about you',
+//     'I am good, now leave me alone'
+// ];
 
-const greetings = [
-    'I am good, how about you',
-    ' I am doing fine what about you',
-    'I am good, now leave me alone'
-];
+// const weather = [
+//     'weather is fine',
+//     'why? you dont go out',
+//     'why do you care?'
+// ]
 
-const weather = [
-    'weather is fine',
-    'why? you dont go out',
-    'why do you care?'
-]
+// const SpeechRecognition =
+//     window.SpeechRecognition || window.webkitSpeechRecognition;
+// const recognition = new SpeechRecognition();
 
+// recognition.onstart = function() {
+//     console.log("You can speak now.");
+// };
 
-const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+// recognition.onresult = function(event) {
+//     // console.log(event);
+//     const current = event.resultIndex;
 
-recognition.onstart = function() {
-    console.log("You can speak now.");
-};
+//     const transcript = event.results[current][0].transcript;
+//     content.textContent = "Output: " + transcript;
+//     readOutLoud(transcript);
 
+// };
 
+// btn.addEventListener('click', () => {
+//     recognition.start();
+// });
 
-recognition.onresult = function(event) {
-    // console.log(event);
-    const current = event.resultIndex;
+// function readOutLoud(message) {
 
-    const transcript = event.results[current][0].transcript;
-    content.textContent = "Output: " + transcript;
-    readOutLoud(transcript);
+//     const speech = new SpeechSynthesisUtterance();
+//     speech.text = 'I dont know';
 
-};
+//     if (message.includes('how are you')) {
+//         const finalText =
+//             greetings[Math.floor(Math.random() * greetings.length)];
+//         speech.text = finalText;
+//     } else if (message.includes('weather')) {
+//         const finalText =
+//             weather[Math.floor(Math.random() * weather.length)];
+//         speech.text = finalText;
+//     }
 
-btn.addEventListener('click', () => {
-    recognition.start();
-});
+//     speech.volume = 1;
+//     speech.rate = 1;
+//     speech.pitch = .5;
 
-function readOutLoud(message) {
+//     window.speechSynthesis.speak(speech);
 
-    const speech = new SpeechSynthesisUtterance();
-    speech.text = 'I dont know';
-
-    if (message.includes('how are you')) {
-        const finalText =
-            greetings[Math.floor(Math.random() * greetings.length)];
-        speech.text = finalText;
-    } else if (message.includes('weather')) {
-        const finalText =
-            weather[Math.floor(Math.random() * weather.length)];
-        speech.text = finalText;
-    }
-
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = .5;
-
-    window.speechSynthesis.speak(speech);
-
-}
+// }
